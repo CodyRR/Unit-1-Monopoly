8e86ba2d2d18e3da0c2e1e15efeef43f5ad5b8ad
CodyRR <codyrobinson28@gmail.com>
2025-11-05 20:46:17 -0600

    Setting up home page, nav menu, and having buttons and links move to seperate paths

--- Simplified-Monopoly/src/layout/Header.jsx
+++ Simplified-Monopoly/src/layout/Header.jsx
@@ -1,9 +1,12 @@
+import NavMenu from "./NavMenu";
+
 const Header = () => {
 
     return (
         <header>
             <div id="header-container">
                 <h1>Simplified Monopoly</h1>
+                <NavMenu />
             </div>
         </header>
     )
