c97c96bbc47c88a99f2176637ae5ea0f1eebc1e0
CodyRR <codyrobinson28@gmail.com>
2025-11-05 19:22:39 -0600

    Setting up some initial files/create Header and Footer templates

---
+++ Simplified-Monopoly/src/layout/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+
+    return (
+        <footer>
+            <div>
+                <h5>Footer stuff</h5>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ Simplified-Monopoly/src/layout/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+
+    return (
+        <header>
+            <div id="header-container">
+                <h1>Simplified Monopoly</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
